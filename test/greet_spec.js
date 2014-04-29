4c4d0d624e86cf28e57da8f311c969ebc480fbca
ShaneH <shane@mbp.local>
2014-04-29 20:22:43 +0800

    warm2

---
+++ test/greet_spec.js
@@ -0,0 +1,9 @@
+var greet = require('greet')
+describe('greet', function() {
+    it("should greet a person by name", function(){
+        expect('hello, Mocha').to.equal(greet('Mocha'));
+    });
+    it("should greet a person flirtatiously if drunk", function(){
+        expect('hello Mocha, you look sexy today').to.equal(greet('Mocha', true));
+    });
+});
