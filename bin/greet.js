7dc579fe1bbbd9a25e5df1bf11f02253952a8b2b
ShaneH <shane@mbp.local>
2014-04-30 21:42:58 +0800

    convert

--- bin/greet.js
+++ bin/greet.js
@@ -1,4 +1,3 @@
 #!/usr/bin/env node
-greet = require('../lib/index.js');
-args = require('minimist')(process.argv.slice(2));
-console.log(greet(args._, args.drunk));
+var command = require("../lib/command");
+command();
